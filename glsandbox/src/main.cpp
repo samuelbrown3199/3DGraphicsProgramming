6a8811b89864b949dd834834e693c8c875a911d6
Samuel Brown <samuelbrown3199@gmail.com>
2020-02-28 01:59:32 +0000

    Lab 4

--- glsandbox/src/main.cpp
+++ glsandbox/src/main.cpp
@@ -5,14 +5,19 @@
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 
+// Needs to be defined before the include in exactly
+// one compilation unit.
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
 const GLchar* vertexShaderSrc =
 "attribute vec3 a_Position;" \
-"attribute vec4 a_Color;   " \
+"attribute vec4 a_TexCoord;   " \
 "                          "
-"varying vec4 v_Color;     " \
+"varying vec4 v_TexCoord;     " \
 "							  " \
 "uniform mat4 u_Projection;		"\
 "uniform mat4 u_Model;			"\
@@ -20,14 +25,16 @@ const GLchar* vertexShaderSrc =
 "void main()                " \
 "{						  " \
 " gl_Position = u_Projection * u_Model * vec4(a_Position, 1.0);" \
-" v_Color = a_Color;           "\
+" v_TexCoord = a_TexCoord;           "\
 "}                          ";
 
 const GLchar* fragmentShaderSrc =
-"varying vec4 v_Color;    "\
+"uniform sampler2D u_Texture;" \
+"varying vec4 v_TexCoord;    "\
 "void main()              "\
 "{                        "\
-" gl_FragColor = v_Color;"\
+" vec4 tex = texture2D(u_Texture, v_TexCoord);"
+" gl_FragColor = tex;"\
 "}";
 
 
@@ -62,13 +69,45 @@ int main(int argc, char* argv[])
         0.0f, -0.5f, 0.0f,
         1.5f, -0.5f, 0.0f
     };
-
-    const GLfloat colors[] = 
+    /*const GLfloat colors[] = 
     {
         1.0f, 0.0f, 0.0f, 1.0f,
         0.0f, 1.0f, 0.0f, 1.0f,
         0.0f, 0.0f, 1.0f, 1.0f
+    };*/
+    const GLfloat textureCoords[] =
+    {
+        0.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        1.0f, 1.0f, 0.0f, 0.0f
     };
+
+    //load the texture
+    int w = 0;
+    int h = 0;
+    unsigned char* data = stbi_load("image.png", &w, &h, NULL, 4);
+    if (!data)
+    {
+        throw std::exception();
+    }
+
+    //create and bind a texture
+    GLuint textureID = 0;
+    glGenTextures(1, &textureID);
+
+    if (!textureID)
+    {
+        throw std::exception();
+    }
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    //upload the image daya to the bound texture unit in the GPU
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    //free the loaded data because we now have a copy on the GPU
+    free(data);
+    //generate mipmap so the texture can be mapped correctly
+    glGenerateMipmap(GL_TEXTURE_2D);
+    //unbind the texture becase we are done operating on it
+    glBindTexture(GL_TEXTURE_2D, 0);
 
     GLuint positionsVboID = 0;
     glGenBuffers(1, &positionsVboID);
@@ -81,7 +120,7 @@ int main(int argc, char* argv[])
     glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    GLuint colorsVboID = 0;
+   /* GLuint colorsVboID = 0;
     glGenBuffers(1, &colorsVboID);
 
     if (!colorsVboID)
@@ -91,6 +130,16 @@ int main(int argc, char* argv[])
 
     glBindBuffer(GL_ARRAY_BUFFER, colorsVboID);
     glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);*/
+
+    GLuint textureVboID = 0;
+    glGenBuffers(1, &textureVboID);
+    if (!textureVboID)
+    {
+        throw std::exception();
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, textureVboID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(textureCoords), textureCoords, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     GLuint vaoId = 0;
@@ -105,7 +154,7 @@ int main(int argc, char* argv[])
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, colorsVboID);
+    glBindBuffer(GL_ARRAY_BUFFER, textureVboID);
     glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(1);
 
@@ -135,7 +184,7 @@ int main(int argc, char* argv[])
     glAttachShader(programID, vertexShaderID);
     glAttachShader(programID, fragmentShaderID);
     glBindAttribLocation(programID, 0, "a_Position");
-    glBindAttribLocation(programID, 1, "a_Color");
+    glBindAttribLocation(programID, 1, "a_TexCoord");
 
     glLinkProgram(programID);
     glGetProgramiv(programID, GL_LINK_STATUS, &success);
@@ -154,7 +203,12 @@ int main(int argc, char* argv[])
 	GLint projectionLoc = glGetUniformLocation(programID, "u_Projection");
 
     bool quit = false;
-	float angle = 0;
+    float angle = 0;
+
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
 
     while (!quit)
     {
@@ -180,15 +234,22 @@ int main(int argc, char* argv[])
 
 		angle += 1.0f;
 
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.65f, 0.65f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(programID);
 
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
+        glBindTexture(GL_TEXTURE_2D, textureID);
         glBindVertexArray(vaoId);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0, 0, -7.5f));
+
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		projection = glm::ortho(0.0f, (float)WINDOW_WIDTH, 0.0f, (float)WINDOW_HEIGHT, 0.0f, 1.0f);
@@ -201,6 +262,7 @@ int main(int argc, char* argv[])
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        glBindTexture(GL_TEXTURE, 0);
         glBindVertexArray(0);
         glUseProgram(0);
 
